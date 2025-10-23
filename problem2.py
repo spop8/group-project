a978d89bd9a2b3501f5ae8206712a5cbdfb474a7
Solena <123829734+spop8@users.noreply.github.com>
2025-10-23 17:52:48 -0500

    Update problem2.py

--- problem2.py
+++ problem2.py
@@ -1,3 +1,26 @@
+'''
+Understand: 
+I have to get a number and calculate the factorial
+Input: number output: factorial
+
+Clues
+- 
+
+Assemble
+INPUT --> ask user for an integer
+
+create FUNCTION factorial that takes 1 parameter (the integer from above)
+create VARIABLE that will be the result
+create FOR LOOP that will iterate over a range of numbers 
+    - the range starts at 1 and goes up to the number + 1 so the number is included  
+    - multiplies the previous result by the next number
+returns the RESULT
+PRINTS --> the factorial/RESULT in a sentence
+
+
+'''
+
+
 num = int(input("Enter a number you would like to know the factorial of: "))
 
 def factorial(num):
