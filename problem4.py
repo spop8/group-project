b2030f44bd4e167bf05491a5ceb427d616755b23
Solena <123829734+spop8@users.noreply.github.com>
2025-10-23 17:54:15 -0500

    Update problem4.py

---
+++ problem4.py
@@ -0,0 +1,12 @@
+def intToString(n):
+    
+     If isinstance(n,int):
+
+       If n % 3 == 0 or n % 3 == 0:
+           print(“FizzBuzz”)
+       else if n % 3 == 0:
+           print(“Fizz”)
+       else If n % 5 == 0:
+           print(“Buzz”) 
+     else:
+     print(“ERROR, input is not an Integer”)
