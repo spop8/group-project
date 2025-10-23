d4b6d8c44ce8670c8300da1cf3e5e33808a7b445
Swaroop Kadiyam <swaroop.kadiyam@gmail.com>
2025-10-23 22:59:27 +0000

    just did psuedo code for problem 1.py

---
+++ problem1.py
@@ -0,0 +1,7 @@
+#U: The problem is I have to make a piece of code that can reverse a statement, for example hello will be replaced with olleh.
+#C: I can use an i for i to loop through each character and I can also use a for loop to help with that. 
+#A: def reversestatementinator(str)
+  #declare a variable to hold the user input
+  # for loop
+  #i for i, take the last index of the users input and place it at the front
+  #if the user uses a number, give an error message
